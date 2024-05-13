bef1dc83d6d710bdd11345c747d6d2b7ffd9f580
Poliana <polianasamonte@gmail.com>
2024-05-13 15:42:50 -0300

    Exercicio 06 implementado :)

--- Lista de Exercicios/P06_Poliana.pl
+++ Lista de Exercicios/P06_Poliana.pl
@@ -1,7 +1,7 @@
 /*P06 (*) Find out whether a list is a palindrome.
 A palindrome can be read forward or backward; e.g. [x,a,m,a,x].*/
 
-palindrome(L1,L2) :- is_palindrome(L1,L2,[]).
+palindrome(L1) :- is_palindrome(L1,L1,[]).
 
-is_palindrome(L1,L1,_) :- !.
-is_palindrome([H|T],L2,Aux) :- 
+is_palindrome([],L1,L1) :- !.
+is_palindrome([H|T],L1,Aux) :- is_palindrome(T,L1,[H|Aux]).
